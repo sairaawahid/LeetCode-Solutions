84fbd671ab04e0204f907b307cd7337ba476d3e9
Sairaawahid <sairaawahid@gmail.com>
2024-11-27 13:17:19 +0500

    Time: 51 ms (36.32%), Space: 29 MB (48.26%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -1,6 +1,7 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
        
+       # check both monotonic increasing and decreasing
         increase, decrease = True, True
 
         for i in range(len(nums) - 1):
@@ -10,3 +11,15 @@ class Solution:
                 decrease = False
 
         return increase or decrease
+
+        '''
+        check monotovic increasing only
+
+        if nums[-1] - nums[0] < 0:
+            nums.reverse()
+        for i range(len(nums) - 1):
+            if not (nums[i] <= nums[i + 1]):
+                return False
+        return True
+        
+        '''
